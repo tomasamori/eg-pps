a3d19c10ad36be2d1325c18fc5cd0894932e7265
matimayor <matias.mayor99@gmail.com>
2024-03-11 16:53:21 -0300

    Add header and footer

--- src/index.php
+++ src/index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema PPS</title>
-</head>
-<body>
+<?php include("header.php") ?>
+
     <h1>
         <?php
             echo "Hola Mundo";
         ?>
     </h1>
-</body>
-</html>
+    
+<?php include("footer.php") ?>
