eee3fc782aeb65ddcb8fd0323758a31a97453e3a
matimayor <matias.mayor99@gmail.com>
2024-03-15 10:48:11 -0300

    Update database script

--- db/eg-pps.sql
+++ db/eg-pps.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: db:3306
--- Tiempo de generación: 12-03-2024 a las 00:56:21
--- Versión del servidor: 8.0.35
--- Versión de PHP: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Mar 15, 2024 at 01:35 PM
+-- Server version: 10.5.20-MariaDB
+-- PHP Version: 7.3.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,235 +18,234 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `eg-pps`
+-- Database: `eg-pps`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `career`
+-- Table structure for table `career`
 --
 
 CREATE TABLE `career` (
-  `career_id` int NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `career`
---
-
-INSERT INTO `career` (`career_id`, `name`) VALUES
-(1, 'Ingeniería en Sistemas de Información');
+  `career_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `document`
+-- Table structure for table `document`
 --
 
 CREATE TABLE `document` (
-  `document_id` int NOT NULL,
+  `document_id` int(11) NOT NULL,
   `path` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
   `correction` text NOT NULL,
   `type` varchar(255) NOT NULL,
-  `spp_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  `spp_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `notification`
+-- Table structure for table `notification`
 --
 
 CREATE TABLE `notification` (
-  `notification_id` int NOT NULL,
-  `sender_id` int NOT NULL,
-  `receiver_id` int NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `notification_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` varchar(255) NOT NULL,
-  `message` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `message` text NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `role`
+-- Table structure for table `role`
 --
 
 CREATE TABLE `role` (
-  `role_id` int NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `role_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `spp`
+-- Table structure for table `spp`
 --
 
 CREATE TABLE `spp` (
-  `spp_id` int NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `spp_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `spp_user`
+-- Table structure for table `spp_user`
 --
 
 CREATE TABLE `spp_user` (
-  `spp_id` int NOT NULL,
-  `supervisor_id` int NOT NULL,
-  `mentor_id` int NOT NULL,
-  `student_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `spp_id` int(11) NOT NULL,
+  `supervisor_id` int(11) NOT NULL,
+  `mentor_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `user_id` int NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `photo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `deleted` tinyint(1) NOT NULL,
-  `career_id` int NOT NULL,
-  `role_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `user_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  `career_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `career`
+-- Indexes for table `career`
 --
 ALTER TABLE `career`
   ADD PRIMARY KEY (`career_id`);
 
 --
--- Indices de la tabla `document`
+-- Indexes for table `document`
 --
 ALTER TABLE `document`
   ADD PRIMARY KEY (`document_id`),
-  ADD KEY `document_spp_id_spp` (`spp_id`);
+  ADD KEY `spp_spp_id` (`spp_id`);
 
 --
--- Indices de la tabla `notification`
+-- Indexes for table `notification`
 --
 ALTER TABLE `notification`
-  ADD PRIMARY KEY (`notification_id`,`sender_id`,`receiver_id`),
-  ADD KEY `users_sender_id_notification` (`sender_id`),
-  ADD KEY `users_receiver_id_notification` (`receiver_id`);
+  ADD PRIMARY KEY (`notification_id`,`receiver_id`),
+  ADD KEY `user_sender_id` (`sender_id`),
+  ADD KEY `user_receiver_id` (`receiver_id`);
 
 --
--- Indices de la tabla `role`
+-- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`role_id`);
 
 --
--- Indices de la tabla `spp`
+-- Indexes for table `spp`
 --
 ALTER TABLE `spp`
   ADD PRIMARY KEY (`spp_id`);
 
 --
--- Indices de la tabla `spp_user`
+-- Indexes for table `spp_user`
 --
 ALTER TABLE `spp_user`
   ADD PRIMARY KEY (`spp_id`,`supervisor_id`,`mentor_id`,`student_id`),
-  ADD KEY `spp_user_supervisor_id_users` (`supervisor_id`),
-  ADD KEY `spp_user_mentor_id_users` (`mentor_id`),
-  ADD KEY `spp_user_student_id_users` (`student_id`);
+  ADD KEY `user_supervisor_id` (`supervisor_id`),
+  ADD KEY `user_mentor_id` (`mentor_id`),
+  ADD KEY `user_student_id` (`student_id`);
 
 --
--- Indices de la tabla `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`),
-  ADD KEY `career_career_id_users` (`career_id`),
-  ADD KEY `role_role_id_users` (`role_id`);
+  ADD KEY `career_career_id` (`career_id`),
+  ADD KEY `role_role_id` (`role_id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `career`
+-- AUTO_INCREMENT for table `career`
 --
 ALTER TABLE `career`
-  MODIFY `career_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `career_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `document`
+-- AUTO_INCREMENT for table `document`
 --
 ALTER TABLE `document`
-  MODIFY `document_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `notification`
+-- AUTO_INCREMENT for table `notification`
 --
 ALTER TABLE `notification`
-  MODIFY `notification_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `role`
+-- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `role_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `spp`
+-- AUTO_INCREMENT for table `spp`
 --
 ALTER TABLE `spp`
-  MODIFY `spp_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `spp_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `document`
+-- Constraints for table `document`
 --
 ALTER TABLE `document`
-  ADD CONSTRAINT `document_spp_id_spp` FOREIGN KEY (`spp_id`) REFERENCES `spp` (`spp_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `document_spp_id_spp` FOREIGN KEY (`spp_id`) REFERENCES `spp` (`spp_id`) ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `notification`
+-- Constraints for table `notification`
 --
 ALTER TABLE `notification`
-  ADD CONSTRAINT `users_receiver_id_notification` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_sender_id_notification` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_receiver_id` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `spp_user`
+-- Constraints for table `spp_user`
 --
 ALTER TABLE `spp_user`
-  ADD CONSTRAINT `spp_user_mentor_id_users` FOREIGN KEY (`mentor_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `spp_user_spp_id_spp` FOREIGN KEY (`spp_id`) REFERENCES `spp` (`spp_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `spp_user_student_id_users` FOREIGN KEY (`student_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `spp_user_supervisor_id_users` FOREIGN KEY (`supervisor_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `spp_spp_id` FOREIGN KEY (`spp_id`) REFERENCES `spp` (`spp_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_mentor_id` FOREIGN KEY (`mentor_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_student_id` FOREIGN KEY (`student_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_supervisor_id` FOREIGN KEY (`supervisor_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `career_career_id_users` FOREIGN KEY (`career_id`) REFERENCES `career` (`career_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `role_role_id_users` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `career_career_id` FOREIGN KEY (`career_id`) REFERENCES `career` (`career_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `role_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
