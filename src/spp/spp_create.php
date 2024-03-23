f3511ba29371da136738591f08081273e3e3c845
matimayor <matias.mayor99@gmail.com>
2024-03-23 08:46:32 -0300

    Update spp create

--- src/spp/spp_create.php
+++ src/spp/spp_create.php
@@ -17,8 +17,16 @@
         $stmt = $conn->prepare($sql);
         $stmt->bind_param('ssssssisss', $organization_name, $organization_email, $organization_phone, $organization_address, $organization_city, $organization_state, $organization_zip, $start_date, $status, $organization_contact);
         if ($stmt->execute()) {
-            $_SESSION['message'] = 'PPS registrada exitosamente. Redirigiendo...';
-            header("Location: spp.php");
+            $spp_id = mysqli_insert_id($conn);
+            $user_id = $_SESSION['user_id'];
+            $query = "INSERT INTO spp_user (spp_id, student_id) VALUES ('$spp_id', '$user_id')";
+            $result = mysqli_query($conn, $query);
+            if (!$result) {
+                die("La consulta ha fallado.");
+            } else {
+            $_SESSION['message'] = 'PPS con ID='.$spp_id.' registrada exitosamente para el usuario con ID=' .$user_id.'. Redirigiendo...';
+            header("Location: spp.php"); 
+            }
         } else {
             $message = 'Hubo un problema al crear tu PPS';
         }
