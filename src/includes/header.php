5ef39bd1292253feb58c2e891e88134a2378abf7
Santiago Tombolini <santitombo@gmail.com>
2024-03-26 16:43:47 -0300

    prof_list fixed, now working correctly

--- src/includes/header.php
+++ src/includes/header.php
@@ -7,6 +7,12 @@
     <!-- BOOSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+<style>
+  body {
+        margin: 0;
+        padding-bottom: 50px; /* Ajusta este valor según la altura del footer */
+    }
+</style>
 <body>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
--- src/professor_list/prof_list.php
+++ src/professor_list/prof_list.php
@@ -1,51 +1,76 @@
 <?php session_start(); ?>
-<?php include("../db.php") ?>
-<?php include("../includes/header.php") ?>
+<?php include ("../db.php") ?>
+<?php include ("../includes/header.php") ?>
 
 <div class="container p-4 bg-light">
     <div class="row">
         <div class="col-md-12">
             <div class="card card-body">
                 <h2 class="text-center mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
-                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0m2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor"
+                        class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0m2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755" />
                     </svg>
                     Profesores Disponibles
                 </h2>
                 <table class="table">
                     <tbody>
                         <?php
-                        $query = "SELECT user_id, user.name, user.email, career.name AS career_name 
-                        FROM user 
-                        INNER JOIN career ON user.career_id = career.career_id 
-                        LEFT JOIN spp_user ON spp_user.mentor_id = user.user_id
-                        WHERE role_id = '2'";
-                        //En caso de que el usuario esté logueado
-                        if (isset($_SESSION['user_id'])) {
-                            $student_id = $_SESSION['user_id'];
-                            $query .= " AND user.career_id IN (SELECT user.career_id FROM user WHERE user.user_id = '$student_id')";
+                        $query = "SELECT role_id FROM role WHERE name = 'Profesor'";
+                        $result = $conn->query($query);
+                        if ($result && $result->num_rows > 0) {
+                            $row = $result->fetch_assoc();
+                            $role_id = $row['role_id'];
+
+                            $query = "SELECT user_id, user.name, user.email, career.name AS career_name, spp.status AS spp_status
+                                            FROM user 
+                                            INNER JOIN career ON user.career_id = career.career_id 
+                                            LEFT JOIN spp_user ON spp_user.mentor_id = user.user_id
+                                            LEFT JOIN spp ON spp_user.spp_id = spp.spp_id
+
+                                            WHERE role_id = $role_id";
+                            //En caso de que el usuario esté logueado
+                            if (isset($_SESSION['user_id'])) {
+                                $student_id = $_SESSION['user_id'];
+                                $query .= " AND user.career_id = (SELECT user.career_id FROM user WHERE user.user_id = '$student_id')";
+                            }
+                            //Trae los docentes que tengan menos de 10 pps
+                            $query .= " GROUP BY 
+                                            user.user_id
+                                            HAVING 
+                                            COALESCE(SUM(spp_status = 'in course'), 0) < 10";
+                                                
+                            echo "hasta aca llega";                    
+                            $result_users = mysqli_query($conn, $query);
+                        } else {
+                            $message = 'No se encontró el rol "Profesor"';
                         }
 
-                        $query .= " GROUP BY user_id
-                                    HAVING COALESCE(COUNT(spp_user.mentor_id), 0) < 10";
-                        $result_users = mysqli_query($conn, $query);
-              
                         while ($row = mysqli_fetch_array($result_users)) { ?>
                             <tr>
-                                <td style="font-size: 20px;"><img src="../img/utn-logo.png" width="45">&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $row['name'] ?></td>
+                                <td style="font-size: 20px;"><img src="../img/utn-logo.png"
+                                        width="45">&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <?php echo $row['name'] ?>
+                                </td>
                                 <td class="text-end">
-                                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $row['user_id']; ?>">
+                                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal<?php echo $row['user_id']; ?>">
                                         Detalles
                                     </button>
                                 </td>
                             </tr>
 
-                            <div class="modal fade" id="exampleModal<?php echo $row['user_id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="exampleModal<?php echo $row['user_id']; ?>" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog modal-dialog-centered">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="exampleModalLabel"><?php echo $row['name'] ?></h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                                <?php echo $row['name'] ?>
+                                            </h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body">
                                             <div class="row">
@@ -75,4 +100,4 @@
     </div>
 </div>
 
-<?php include("../includes/footer.php") ?>
+<?php include ("../includes/footer.php") ?>
