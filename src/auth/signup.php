74d3d9fbaec760938a4ba4e34fd939ab02d08e5b
tomasamori <tomas.amori@gmail.com>
2024-03-27 18:51:29 -0300

    Update sign up validations

--- src/auth/signup.php
+++ src/auth/signup.php
@@ -10,37 +10,49 @@ require '../db.php';
 
 $message = '';
 
-$emailValue = isset($_POST['email']) ? $_POST['email'] : '';
-$passwordValue = isset($_POST['password']) ? $_POST['password'] :'';
-$confirmPasswordValue = isset($_POST['confirm_password']) ? $_POST['confirm_password'] : '';
-$nameValue = isset($_POST['name']) ? $_POST['name'] : '';
-$careerIdValue = isset($_POST['career_id']) ? $_POST['career_id'] : '';
+$emailValue = isset ($_POST['email']) ? $_POST['email'] : '';
+$passwordValue = isset ($_POST['password']) ? $_POST['password'] : '';
+$confirmPasswordValue = isset ($_POST['confirm_password']) ? $_POST['confirm_password'] : '';
+$nameValue = isset ($_POST['name']) ? $_POST['name'] : '';
+$careerIdValue = isset ($_POST['career_id']) ? $_POST['career_id'] : '';
 
 if (!empty ($_POST['email']) && !empty ($_POST['password']) && !empty ($_POST['confirm_password'])) {
-    if ($_POST['password'] !== $_POST['confirm_password']) {
-        $message = 'Las contraseñas no coinciden';
+    $query = "SELECT email FROM user WHERE email = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('s', $_POST['email']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result && $result->num_rows > 0) {
+        $message = 'El correo electrónico ya está registrado';
     } else {
-        $query = "SELECT role_id FROM role WHERE name = 'Alumno'";
-        $result = $conn->query($query);
-
-        if ($result && $result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $role_id = $row['role_id'];
-
-            $sql = "INSERT INTO user (email, password, name, career_id, role_id) VALUES (?, ?, ?, ?, ?)";
-            $stmt = $conn->prepare($sql);
-
-            $hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-
-            $stmt->bind_param('ssssi', $_POST['email'], $hashed_password, $_POST['name'], $_POST['career_id'], $role_id);
-
-            if ($stmt->execute()) {
-                $message = 'Usuario creado exitosamente. Redirigiendo...';
-            } else {
-                $message = 'Hubo un problema al crear tu cuenta';
-            }
+        if ($_POST['password'] !== $_POST['confirm_password']) {
+            $message = 'Las contraseñas no coinciden';
+        } elseif (strlen($_POST['password']) < 8 || !preg_match('/[a-zA-Z]/', $_POST['password']) || !preg_match('/\d/', $_POST['password'])) {
+            $message = 'La contraseña debe tener al menos 8 caracteres y contener al menos 1 letra y 1 número';
         } else {
-            $message = 'No se encontró el rol "Alumno"';
+            $query = "SELECT role_id FROM role WHERE name = 'Alumno'";
+            $result = $conn->query($query);
+
+            if ($result && $result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $role_id = $row['role_id'];
+
+                $sql = "INSERT INTO user (email, password, name, career_id, role_id) VALUES (?, ?, ?, ?, ?)";
+                $stmt = $conn->prepare($sql);
+
+                $hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+                $stmt->bind_param('ssssi', $_POST['email'], $hashed_password, $_POST['name'], $_POST['career_id'], $role_id);
+
+                if ($stmt->execute()) {
+                    $message = 'Usuario creado exitosamente. Redirigiendo...';
+                } else {
+                    $message = 'Hubo un problema al crear tu cuenta';
+                }
+            } else {
+                $message = 'No se encontró el rol "Alumno"';
+            }
         }
     }
 }
@@ -52,34 +64,40 @@ include ('../includes/header.php'); ?>
         <div class="col-md-3">
             <div class="card card-body text-center shadow-lg">
                 <form action="signup.php" method="POST">
-                    <img src="../img/utn-logo.png" alt="Logo UTN" class="navbar-brand-img mt-4 mb-4 img-fluid" style="max-height: 70px;">
+                    <img src="../img/utn-logo.png" alt="Logo UTN" class="navbar-brand-img mt-4 mb-4 img-fluid"
+                        style="max-height: 70px;">
                     <div class="form-group m-2">
-                        <input type="email" name="email" class="form-control" placeholder="Email" autofocus required value="<?php echo $emailValue; ?>">
+                        <input type="email" name="email" class="form-control" placeholder="Email" autofocus required
+                            value="<?php echo $emailValue; ?>">
                     </div>
                     <div class="form-group m-2">
-                        <input type="password" name="password" class="form-control" placeholder="Contraseña" required value="<?php echo $passwordValue; ?>">
+                        <input type="password" name="password" class="form-control" placeholder="Contraseña" required
+                            value="<?php echo $passwordValue; ?>">
                     </div>
                     <div class="form-group m-2">
-                        <input type="password" name="confirm_password" class="form-control" placeholder="Confirme su Contraseña" required value="<?php echo $confirmPasswordValue; ?>">
+                        <input type="password" name="confirm_password" class="form-control"
+                            placeholder="Confirme su Contraseña" required value="<?php echo $confirmPasswordValue; ?>">
                     </div>
                     <div class="form-group m-2">
-                        <input type="text" name="name" class="form-control" placeholder="Nombre y Apellido" required value="<?php echo $nameValue; ?>">
+                        <input type="text" name="name" class="form-control" placeholder="Nombre y Apellido" required
+                            value="<?php echo $nameValue; ?>">
                     </div>
                     <?php
-                        $query = "SELECT * FROM career";
-                        $result_career = mysqli_query($conn, $query);
+                    $query = "SELECT * FROM career";
+                    $result_career = mysqli_query($conn, $query);
                     ?>
                     <div class="form-group m-2">
                         <select name="career_id" class="form-control" required>
                             <option value="">Seleccione una carrera</option>
                             <?php while ($row = mysqli_fetch_assoc($result_career)) { ?>
-                                <option value="<?php echo $row['career_id']; ?>" <?php if ($row['career_id'] == $careerIdValue) echo 'selected'; ?>>
+                                <option value="<?php echo $row['career_id']; ?>" <?php if ($row['career_id'] == $careerIdValue)
+                                       echo 'selected'; ?>>
                                     <?php echo $row['name']; ?>
                                 </option>
                             <?php } ?>
                         </select>
                     </div>
-                    <?php if (!empty($message)): ?>
+                    <?php if (!empty ($message)): ?>
                         <br>
                         <?php if ($message === 'Usuario creado exitosamente. Redirigiendo...'): ?>
                             <div class="row justify-content-center align-items-center">
@@ -90,25 +108,26 @@ include ('../includes/header.php'); ?>
                                 </div>
                             </div>
                         <?php else: ?>
-                        <div class="row justify-content-center align-items-center">
-                            <div class="col-md-10">
-                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                    <?= $message ?>
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-10">
+                                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                        <?= $message ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         <?php endif; ?>
                     <?php endif; ?>
 
                     <?php if ($message === 'Usuario creado exitosamente. Redirigiendo...'): ?>
                         <script>
-                            setTimeout(function() {
+                            setTimeout(function () {
                                 window.location.href = 'login.php';
                             }, 4000);
                         </script>
                     <?php endif; ?>
                     <br>
-                    <input type="submit" class="btn btn-success btn-block mx-auto d-block" name="signup" value="Registrarse">
+                    <input type="submit" class="btn btn-success btn-block mx-auto d-block" name="signup"
+                        value="Registrarse">
                 </form>
             </div>
         </div>
