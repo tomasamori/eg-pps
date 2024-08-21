8ddea2c073a6d1f4940b2f9eddbd30129e0ce07e
tomasamori <tomas.amori@gmail.com>
2024-08-21 18:18:38 -0300

    Navbar restyling

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,26 +1,38 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema PPS</title>
-    <!-- BOOSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- FONT AWESOME -->
-    <script src="https://kit.fontawesome.com/cafe638f84.js" crossorigin="anonymous"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistema PPS</title>
+  <!-- Bootstrap 5.3.3 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/cafe638f84.js" crossorigin="anonymous"></script>
 </head>
 <style>
   body {
-        margin: 0;
-        padding-bottom: 50px; /* Ajusta este valor según la altura del footer */
-    }
+    margin: 0;
+    padding-bottom: 50px;
+    /* Ajusta este valor según la altura del footer */
+  }
+
+  .nav-link {
+    color: black;
+  }
+
+  .nav-link:hover {
+    color: gray;
+  }
 </style>
+
 <body>
 
-<?php
+  <?php
   require_once __DIR__ . '/../db.php';
 
-  function getUnreadNotifications($userId) {
+  function getUnreadNotifications($userId)
+  {
     global $conn;
     $sql = "SELECT COUNT(*) AS total FROM notification WHERE receiver_id = ? AND status = 'No leída' AND deleted = 0";
     $stmt = $conn->prepare($sql);
@@ -36,8 +48,104 @@
   }
   ?>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-  <div class="container-fluid">
+
+  <nav class="navbar navbar-expand-lg shadow-sm sticky-top" style="background-color: #ffffff;">
+
+    <div class="container">
+
+      <a class="navbar-brand" href="../index.php">
+        <img src="../img/utn_logo.png" alt="Logo de la Universidad Tecnológica Nacional" height="40">
+      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 fw-bold">
+          <li class="nav-item ms-4 me-2">
+            <a class="nav-link" href="../professor_list/prof_list.php">Profesores</a>
+          </li>
+          <li class="nav-item ms-2 me-4">
+            <a class="nav-link" href="#">PPS</a>
+          </li>
+        </ul>
+
+        <?php if (isset($_SESSION['user_id'])): ?>
+          <div class="dropstart me-2">
+            <button type="button" class="btn btn-dark btn-sm rounded-circle" id="notificationDropdown" data-bs-toggle="dropdown" aria-expanded="false" title="Notificaciones">
+              <i class="fa-solid fa-bell"></i>
+              <?php if ($unreadNotifications > 0): ?>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  <?= $unreadNotifications ?>
+                  <span class="visually-hidden">Notificaciones no leídas</span>
+                </span>
+              <?php endif; ?>
+            </button>
+            <ul class="dropdown-menu text-truncated" aria-labelledby="notificationDropdown">
+              <li>
+                <h6 class="dropdown-header">Notificaciones</h6>
+              </li>
+              <?php
+              $sql = "SELECT * FROM notification WHERE receiver_id = ? AND deleted = 0 ORDER BY timestamp DESC";
+              $stmt = $conn->prepare($sql);
+              $stmt->bind_param("i", $_SESSION["user_id"]);
+              $stmt->execute();
+              $result = $stmt->get_result();
+              if ($result->num_rows === 0): ?>
+                <li><a class='dropdown-item fw-lighter'><small>No hay notificaciones</small></a></li>
+                <?php else:
+                while ($row = $result->fetch_assoc()): ?>
+                  <div class="d-flex align-items-center justify-content-between">
+                    <button class="dropdown-item notification-link" data-notification-id="<?php echo $row["notification_id"]; ?>">
+                      <?php if ($row["status"] === "No leída"): ?>
+                        <strong><small class="d-inline-block text-truncate" style="max-width: 350px;">
+                            <?php echo $row["message"]; ?>
+                          </small></strong>
+                      <?php else: ?>
+                        <small class="d-inline-block text-truncate" style="max-width: 300px;">
+                          <?php echo $row["message"]; ?>
+                        </small>
+                      <?php endif; ?>
+                    </button>
+                    <div class="text-align-end ms-2 me-2">
+                      <button class="btn btn-outline-secondary mb-1 delete-notification" data-notification-id="<?php echo $row["notification_id"]; ?>" title="Eliminar notificación">
+                        <i class="fa-solid fa-trash fa-xs" style="color: #ff0000;"></i>
+                      </button>
+                    </div>
+                  </div>
+                <?php endwhile; ?>
+              <?php endif; ?>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <div class="text-start">
+                <a id="mark-all-as-read" class="btn btn-link text-secondary-emphasis" href="#"><small>Marcar todas como leídas</small></a>
+              </div>
+              </li>
+            </ul>
+          </div>
+
+          <div class="nav-item dropstart">
+            <button type="button" class="btn btn-dark btn-sm rounded-circle" data-bs-toggle="dropdown" aria-expanded="false" title="Mi Cuenta">
+              <i class="fa-solid fa-user"></i>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item " href="../auth/logout.php">Cerrar Sesión</a></li>
+              <li><a class="dropdown-item" href="../user/user_edit.php">Editar Perfil</a></li>
+            </ul>
+          </div>
+
+        <?php else: ?>
+          <a class="btn btn-light me-2" href="../auth/login.php">Acceder</a>
+          <a class="btn btn-dark" href="../auth/signup.php">Registrarse</a>
+        <?php endif; ?>
+      </div>
+
+    </div>
+  </nav>
+  <!--
+<nav class="navbar navbar-expand-lg bd-navbar sticky-top">
+  <div class="container-fluid ms-4 me-4">
     <a class="navbar-brand" href="../index.php">
         <img src="../img/logo - utn.png" alt="Logo-UTN" class="navbar-brand-img mr-2 img-fluid" style="max-height: 60px;">
     </a>
@@ -84,15 +192,15 @@
             <ul class="dropdown-menu text-truncated" aria-labelledby="notificationDropdown">
               <li><h6 class="dropdown-header">Notificaciones</h6></li>
               <?php
-                $sql = "SELECT * FROM notification WHERE receiver_id = ? AND deleted = 0 ORDER BY timestamp DESC";
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("i", $_SESSION["user_id"]);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                if ($result->num_rows === 0): ?>
+              $sql = "SELECT * FROM notification WHERE receiver_id = ? AND deleted = 0 ORDER BY timestamp DESC";
+              $stmt = $conn->prepare($sql);
+              $stmt->bind_param("i", $_SESSION["user_id"]);
+              $stmt->execute();
+              $result = $stmt->get_result();
+              if ($result->num_rows === 0): ?>
                   <li><a class='dropdown-item fw-lighter'><small>No hay notificaciones</small></a></li>
                 <?php else:
-                  while ($row = $result->fetch_assoc()): ?>
+                while ($row = $result->fetch_assoc()): ?>
                     <div class="d-flex align-items-center justify-content-between">
                     <button class="dropdown-item notification-link" data-notification-id="<?php echo $row["notification_id"]; ?>">
                       <?php if ($row["status"] === "No leída"): ?>
@@ -255,4 +363,4 @@
         });
     });
 
-</script>
+</script> -->
