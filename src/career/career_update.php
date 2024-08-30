a93ec112840de5af09504c41d7ba9502c5336a6d
matimayor <matias.mayor99@gmail.com>
2024-08-30 16:24:42 -0300

    Update CRUD career

--- src/career/career_update.php
+++ src/career/career_update.php
@@ -12,7 +12,7 @@ if (isset($_GET['career_id'])) {
     }
 }
 
-if (isset($_POST['update'])){
+if (isset($_POST['update'])) {
     $career_id = $_GET['career_id'];
     $name = $_POST['name'];
 
@@ -25,20 +25,61 @@ if (isset($_POST['update'])){
 }
 ?>
 
+<style>
+    body {
+        background-image: url('../img/auth-bg.jpg');
+    }
+
+    .form-control:focus {
+        border-color: #3aa661;
+        box-shadow: none;
+    }
+
+    .card {
+        background-color: #ffffff;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        overflow: hidden;
+    }
+
+    .card-header {
+        color: white;
+        background-color: #3aa661;
+        font-family: 'Arial', sans-serif;
+        font-weight: bold;
+        padding: 7px;
+    }
+
+    .btn-success {
+        background-color: #3aa661;
+        border-color: #3aa661;
+    }
+
+    .btn-success:hover {
+        background-color: #339a4e;
+        border-color: #339a4e;
+    }
+</style>
+
 <?php include("../includes/header.php") ?>
 <br>
 <div class="container">
     <div class="row">
         <div class="col-md-4 mx-auto">
-            <div class="card card-body">
-                <form action="career_update.php?career_id=<?php echo $_GET['career_id']; ?>" method="POST">
-                    <div class="form-group m-2">
-                        <input type="text" name="name" value="<?php echo $name ?>" class="form-control" placeholder="Actualizar nombre de la Carrera">
-                    </div>
-                    <button class="btn btn-success btn-block mx-auto d-block" name="update">
-                        Editar
-                    </button>
-                </form>
+            <div class="card text-center">
+                <div class="card-header">
+                    Editar Carrera
+                </div>
+                <div class="card-body">
+                    <form action="career_update.php?career_id=<?php echo $_GET['career_id']; ?>" method="POST">
+                        <div class="form-group m-2">
+                            <input type="text" name="name" value="<?php echo $name ?>" class="form-control" placeholder="Actualizar nombre de la Carrera" required>
+                        </div>
+                        <button class="btn btn-success btn-block mx-auto d-block" name="update">
+                            Editar
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
